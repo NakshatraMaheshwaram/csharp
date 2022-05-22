aec93dff67a14450979303f3c83a266525710f64
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-05-22 21:23:52 +0530

    firstcommit

--- Program.cs
+++ Program.cs
@@ -4,39 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReadFile
+namespace BankingSystem1
 {
-    internal class Readfile
+    internal class SavingsAccount : Account
     {
-        public void ReadFile()
+        public override string AccountName()
         {
-            FileStream fileStrObj = new FileStream(@"C:\Users\hp\Documents\TrainningRepo\CsharpRepo\FileHandlingReadFile\networklog.txt", FileMode.Open, FileAccess.Read);
-            StreamReader strReadObj = new StreamReader(fileStrObj);
-            Console.WriteLine("Id" + "\t" + "Source" + "\t" + "\tDestination" + "\t" + "Date" + "\t" + "\tTime" + "\t" + "\tStatus" + "\t" + "Network");
-            while (strReadObj.Peek() > 0)
-            {
-                string line = strReadObj.ReadLine();
-                if (line != "")
-                {
-                    string[] myStrs = line.Split(':');
-                    if (myStrs.Length > 3)
-                    {
-                        string[] mydatetime = myStrs[1].Split(' ');
-                        Console.Write(mydatetime[0] + "\t" + mydatetime[1] + ":" + myStrs[2] + ":" + myStrs[3] + "\t");
-                    }
-                    else
-                    {
-                        Console.Write(myStrs[1] + "\t");
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine();
-                }
-            }
-            Console.WriteLine("\nRead operation completed");
-            Console.ReadLine();
+            return ("Ravi");
+        }
+        public override int AccountNumber()
+        {
+            return (14111198120181);
+        }
+        public override decimal AccountBalance()
+        {
+            return (14300);
+        }
+        public override string ATM()
+        {
+            return ("ATM is avilable");
         }
     }
 }
