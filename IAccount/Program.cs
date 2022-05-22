af2fdaa8b9ecf0ca265d2b5d9c26a95e477454c5
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-05-22 20:19:54 +0530

    firstcommit

---
+++ IAccount/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    public interface IAccount
+    {
+        string AccontName();
+        int AccountNumber();
+        decimal AccountBalance();
+
+    }
+    interface IAccountTransaction
+    {
+        int Transaction();
+    }
+    public class SalaryAccount : IAccount, IAccountTransaction
+    {
+        public string AccontName()
+        {
+            return "Nakshatra";
+        }
+
+        public decimal AccountBalance()
+        {
+            return 50000;
+        }
+
+        public int AccountNumber()
+        {
+            return 181229;
+        }
+
+        int Transaction()
+        {
+            return 1430;
+        }
+
+        int IAccountTransaction.Transaction()
+        {
+            return 1430;
+        }
+    }
+
+}
