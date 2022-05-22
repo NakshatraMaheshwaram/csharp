67f34720636ccad430245d6c94d8c62071901fb1
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-05-22 21:11:36 +0530

    firstcommit

--- Program.cs
+++ Program.cs
@@ -4,19 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileHandlingReadFile1
+namespace ReadFile
 {
-    internal class Program
+    internal class Readfile
     {
-        static void Main(string[] args)
+        public void ReadFile()
         {
-            Readfile readfile = new Readfile();
-            readfile.ReadFile();
-            readfile.Success();
-            readfile.Failed();
-            readfile.Missed();
-            readfile.Dailed();
-        }
+            FileStream fileStrObj = new FileStream(@"C:\Users\hp\Documents\TrainningRepo\CsharpRepo\FileHandlingReadFile\networklog.txt", FileMode.Open, FileAccess.Read);
+            StreamReader strReadObj = new StreamReader(fileStrObj);
+            Console.WriteLine("Id" + "\t" + "Source" + "\t" + "\tDestination" + "\t" + "Date" + "\t" + "\tTime" + "\t" + "\tStatus" + "\t" + "Network");
+            while (strReadObj.Peek() > 0)
+            {
+                string line = strReadObj.ReadLine();
+                if (line != "")
+                {
+                    string[] myStrs = line.Split(':');
+                    if (myStrs.Length > 3)
+                    {
+                        string[] mydatetime = myStrs[1].Split(' ');
+                        Console.Write(mydatetime[0] + "\t" + mydatetime[1] + ":" + myStrs[2] + ":" + myStrs[3] + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(myStrs[1] + "\t");
+                    }
 
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("\nRead operation completed");
+            Console.ReadLine();
+        }
     }
 }
