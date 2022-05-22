7d0c2139504bd09cc66f6f6fc1cecf2f099d4be4
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-05-22 20:31:58 +0530

    firstcommit

--- Program.cs
+++ Program.cs
@@ -4,34 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileHandlingHandson
+namespace FileHandlingReadFile1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            FileHandlingClass fileobj = new FileHandlingClass();
-            Console.WriteLine("Enter the User Details :-");
-            fileobj.User_Module();
-            Console.WriteLine("\n");
-            Console.WriteLine();
-
-            Console.WriteLine("Enter the Category Details :-");
-            fileobj.Category_Module();
-            Console.WriteLine("\n");
-            Console.WriteLine();
-            Console.WriteLine("Enter the Product Details :-");
-            fileobj.Product_Module();
-            Console.WriteLine("\n");
-
-            Console.WriteLine();
-            Console.WriteLine("Enter the PURCHASE AND Sales Details :-");
-            fileobj.Purchase_and_Sales_Module();
-            Console.WriteLine("\nEnter any key to continue");
-
-
-
-            Console.ReadKey();
+            Readfile readfile = new Readfile();
+            readfile.ReadFile();
+            readfile.Success();
+            readfile.Failed();
+            readfile.Missed();
+            readfile.Dailed();
         }
+
     }
 }
