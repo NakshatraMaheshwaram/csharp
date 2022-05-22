fab30a4a6530f7b5a44e50dbc99120c5a7334965
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-05-22 20:10:12 +0530

    firstcommit

---
+++ BankingSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    internal class CurrentAccount : Account
+    {
+        public override string AccountName()
+        {
+            return "Nakshatra";
+        }
+        public override int AccountNumber()
+        {
+            return 12345;
+        }
+        public override decimal AccountBalance()
+        {
+            return 40000;
+        }
+        public override string ATM()
+        {
+            return "ATM is available";
+        }
+    }
+}
+
