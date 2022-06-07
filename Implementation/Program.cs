785e4617bb3395599fcd5009015d36d41ac9155e
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-06-07 15:34:41 +0530

    Firstcommit

--- Implementation/Program.cs
+++ Implementation/Program.cs
@@ -4,52 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Implementation
+namespace CalculatorLib
 {
-    internal class Program
+    public class IcalculatorLib
     {
-        static void Main(string[] args)
+       public double Add(double a, double b)
         {
-            //    JaggedArray jaggedArray = new JaggedArray();
-            //    jaggedArray.StudentArray();
-            //    Console.WriteLine("\n\n************************ REF&OUT START ************************");
-            //    RefAndOut refAndOut = new RefAndOut();
-            //    refAndOut.RefFunction();
-            //    refAndOut.OutFunction();
-            //    Console.WriteLine("************************ REF&OUT END ************************");
-
-            //Checked Unchecked
-            //CheckedUnchecked checkedUnchecked = new CheckedUnchecked();
-            //checkedUnchecked.uncheckedfun();
-            //checkedUnchecked.checkedfun();
-
-            //Nested Class
-            NestedClasses.Outer_class.Inner_class inner_Class = new NestedClasses.Outer_class.Inner_class();
-            inner_Class.Method1();
-
-            //Nullable 
-            Console.WriteLine("Null Keyword:-");
-            Nullable nullableDemo = new Nullable();
-            nullableDemo.PerformNullableExample();
-
-            //Method hiding
-            Console.WriteLine("Method Hiding:-");
-            MethodHinding obj = new MethodHinding();
-            obj.member();
-
-            //tuples
-            Console.WriteLine("Tupples:-");
-            Tuples tup = new Tuples();
-            tup.SetTupleMethod();
-
-            //struct
-            Console.WriteLine("Struct And Classes:-");
-            STRUCTURE usingStruct = new STRUCTURE();
-            usingStruct.PerFormStruct();
-            usingStruct.StructCanNotBeNullAssign();
-
-            Console.ReadLine();
+            return a + b;
         }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
     }
 }
-        
+
---
+++ Implementation/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using CalculatorLib;
+using Xunit;
+using System;
+
+namespace MyTestProject
+{
+    public class UnitTest1
+    {
+        ICalculatorLib ICal = new ICalculatorLib();
+        [Fact]
+
+        public void Test_Addition()
+        {
+            var a = 10;
+            var b = 20;
+            var result = ICal.Add(a, b);
+            Assert.Equal(30, result);
+        }
+        [Fact]
+        public void Test_Substraction()
+        {
+            var a = 100;
+            var b = 50;
+            var result = ICal.Sub(a, b);
+            Assert.Equal(50, result);
+        }
+        [Fact]
+        public void Test_Multiply()
+        {
+            var a = 10;
+            var b = 10;
+            var result = ICal.Multiply(a, b);
+            Assert.Equal(100, result);
+        }
+        [Fact]
+        public void Test_Divide()
+        {
+            var a = 20;
+            var b = 4;
+            var result = ICal.Divide(a, b);
+            Assert.Equal(5, result);
+        }
+    }
+
+
+}
