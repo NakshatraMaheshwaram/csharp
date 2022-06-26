fb0ef2437538c8f5f2d888d8cd719ff05a4e0250
NakshatraMaheshwaram <101546259+NakshatraMaheshwaram@users.noreply.github.com>
2022-06-26 11:33:10 +0530

    InitialCommit

---
+++ Entities/EmployeeEducation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data.Entities
+{
+    public class EmployeeEducation
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [System.ComponentModel.DataAnnotations.Key]
+
+        public int ID { get; set; }
+
+        public string? CourseName { get; set; }
+
+        public string? UniversityName { get; set; }
+
+
+        public int PassingYear { get; set; }
+
+        public int MarksPercentage { get; set; }
+
+    }
+}
+    
